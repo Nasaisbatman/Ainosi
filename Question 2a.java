ee386ce481553d7dadc3e579ad71d44a5b787eb5
Nasaisbatman <itsnoteasybeingsege@gmail.com>
2025-01-24 16:30:13 +0100

    Question 2a.java

--- Question 2a.java
+++ Question 2a.java
@@ -1,6 +1,6 @@
 public class PatternStarsEquals {
     public static void main(String[] args) {
-        for (int i = 0; i < 6; i++) { // 6 rows
+        for (int i = 0; i < 6; i++) {
             if (i < 3) {
                 System.out.println("* * * * = = = = = = =");
             } else {
